75ac87f4eecce49d3d5a8752796c877f05221399
Petr Bena <benapetr@gmail.com>
2013-11-19 13:34:35 +0100

    debug

--- CluebotRelay/cluebotrelay.cpp
+++ CluebotRelay/cluebotrelay.cpp
@@ -49,6 +49,7 @@ void CluebotRelay::run()
 
     this->timer->start(200);
     connect(this->timer, SIGNAL(timeout()), this, SLOT(OnTick()));
+    this->cluenet->Join("#cluebotng-spam");
     this->cluenet->Join("#wikipedia-van");
     this->tm->Join("#en.wikipedia.huggle");
 }
